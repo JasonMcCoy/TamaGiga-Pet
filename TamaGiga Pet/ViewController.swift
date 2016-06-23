267729f17508a87f8db4bfbd83fdae1d1e6042b9
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-06-22 21:38:06 -0700

    Setup UI and Animation for Golem
    Now I will add more logic so the golem will die if not fed. I will also
    make sound effects play when the heart and food is dragged on top of
    the golem.

--- TamaGiga Pet/ViewController.swift
+++ TamaGiga Pet/ViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imgArray = [UIImage]()
+        for x in 1...4 {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 1.0
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
     }
 
 
