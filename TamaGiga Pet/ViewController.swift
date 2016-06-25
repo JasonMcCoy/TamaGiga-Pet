b1568adb213a19d987da190ffe96f26a87508f0e
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-06-25 00:10:55 -0700

    Death Animation implemented
    I implemented a death animation for the golem after waiting approx 5
    seconds if you don’t feed him/her.

--- TamaGiga Pet/ViewController.swift
+++ TamaGiga Pet/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
+    @IBOutlet weak var penaltyOneImg: UIImageView!
+    @IBOutlet weak var penaltyTwoImg: UIImageView!
+    @IBOutlet weak var penaltyThreeImg: UIImageView!
     
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: Timer!
 
     override func viewDidLoad() {
         
@@ -22,12 +32,52 @@ class ViewController: UIViewController {
         foodImg.dropTarget = monsterImg
         heartImg.dropTarget = monsterImg
         
+        penaltyOneImg.alpha = DIM_ALPHA
+        penaltyTwoImg.alpha = DIM_ALPHA
+        penaltyThreeImg.alpha = DIM_ALPHA
+        
         NotificationCenter.default().addObserver(self, selector: #selector(itemDroppedOnCharacter), name: "onTargetDropped", object: nil)
+        
+        startTimer()
         
     }
     
     func itemDroppedOnCharacter(notify: AnyObject) {
-        print("PRINT ITEM DROPPED ON THE CHARACTER")
+        
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+            timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        
+        penalties += 1
+            
+        if penalties == 1 {
+            penaltyOneImg.alpha = OPAQUE
+            penaltyTwoImg.alpha = DIM_ALPHA
+        } else if penalties == 2 {
+            penaltyTwoImg.alpha = OPAQUE
+            penaltyThreeImg.alpha = DIM_ALPHA
+        } else if penalties >= 3 {
+            penaltyThreeImg.alpha = OPAQUE
+        } else {
+            penaltyOneImg.alpha = DIM_ALPHA
+            penaltyTwoImg.alpha = DIM_ALPHA
+            penaltyThreeImg.alpha = DIM_ALPHA
+        }
+        if penalties >= MAX_PENALTIES {
+            gameOver()
+        }
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
     }
 
 }
